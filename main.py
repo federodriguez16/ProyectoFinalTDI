2016eeec9be708d76b458fa7f64e298e14a51a2c
Federico Rodriguez <federf2018@live.com>
2023-11-13 17:20:32 -0300

    feat: file organization

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
 import os
+import shutil
 from werkzeug.utils import secure_filename
 from markupsafe import escape
 from procesamiento import crop, scale
 
 UPLOAD_FOLDER = 'static/uploads/'
+
+id = 1
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -41,27 +44,39 @@ def upload_file():
 
         # En caso de que se haya introducido una imagen de manera correcta se guarda la imagen
         if file and allowed_file(file.filename):
+            global id
             filename = secure_filename(file.filename)
-            print(filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
+            ext = filename.split('.')
+            nombre = f"{id}.{ext[1]}"
+            shutil.copy(f"static/uploads/{filename}",
+                        f"static/processing/{nombre}")
+            id += 1
             # Opcion 1 -- crop
             if request.form['x'] != '':
                 try:
-                    crop(filename, int(float(request.form['x'])), int(float(request.form['y'])), int(
+                    crop(nombre, int(float(request.form['x'])), int(float(request.form['y'])), int(
                         float(request.form['ancho'])), int(float(request.form['alto'])))
                 except:
                     print("Hubo un error con el crop")
 
             # Opcion 2 -- scale
             if request.form['scale'] != '':
-                scale(filename, int(float(request.form['scale'])))
+                scale(nombre, int(float(request.form['scale'])))
 
             # Opcion 3 -- deblur
             if request.form['porcentaje'] == '1':
                 print("deblur activado")
 
-            return render_template('index.html', filename=filename)
+            os.rename(f"static/processing/{nombre}",
+                      f"static/downloads/{nombre}")
+            return redirect(url_for('descargar'))
+
+
+@app.route('/descarga.html')
+def descargar():
+    return render_template('descarga.html')
 
 
 @app.route('/user/<username>')
--- procesamiento.py
+++ procesamiento.py
@@ -5,15 +5,19 @@ from matplotlib.pyplot import imshow, figure, subplot, show
 
 
 def crop(img, x, y, ancho, alto):
-    image = cv2.imread(f"static/uploads/{img}")
+    image = cv2.imread(f"static/processing/{img}")
     crop_image = image[y:y+alto, x:x+ancho]
-    cv2.imwrite(f"static/uploads/{img}", crop_image)
+    cv2.imwrite(f"static/processing/{img}", crop_image)
     return 0
 
 
 def scale(img, porcentajeEscala):
-    image = cv2.imread(f"static/uploads/{img}")
-    scale_image = cv2.resize(
-        image, None, fx=porcentajeEscala/100, fy=porcentajeEscala/100)
-    cv2.imwrite(f"static/uploads/{img}", scale_image)
+    image = cv2.imread(f"static/processing/{img}")
+    if porcentajeEscala < 100:
+        scale_image = cv2.resize(
+            image, None, fx=porcentajeEscala/100, fy=porcentajeEscala/100)
+    else:
+        scale_image = cv2.resize(
+            image, None, fx=porcentajeEscala/100, fy=porcentajeEscala/100, interpolation=cv2.INTER_AREA)
+    cv2.imwrite(f"static/processing/{img}", scale_image)
     return 0
