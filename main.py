973321d7725e8205e743afda5d500a7e07089e2c
Federico Rodriguez <federf2018@live.com>
2023-11-13 15:32:08 -0300

    fix: error correction

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, flash, request, redirect, url_for
 import os
 from werkzeug.utils import secure_filename
 from markupsafe import escape
-from procesamiento import crop
+from procesamiento import crop, scale
 
 UPLOAD_FOLDER = 'static/uploads/'
 
@@ -26,22 +26,41 @@ def hello_world():
 def upload_file():
     if request.method == 'POST':
         print(request.form)
+
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
+
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+
+        # En caso de que se haya introducido una imagen de manera correcta se guarda la imagen
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             print(filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            crop(filename, int(float(request.form['x'])), int(float(request.form['y'])), int(float(
-                request.form['ancho'])), int(float(request.form['alto'])))
+
+            # Opcion 1 -- crop
+            if request.form['x'] != '':
+                try:
+                    crop(filename, int(float(request.form['x'])), int(float(request.form['y'])), int(
+                        float(request.form['ancho'])), int(float(request.form['alto'])))
+                except:
+                    print("Hubo un error con el crop")
+
+            # Opcion 2 -- scale
+            if request.form['scale'] != '':
+                scale(filename, int(float(request.form['scale'])))
+
+            # Opcion 3 -- deblur
+            if request.form['porcentaje'] == '1':
+                print("deblur activado")
+
             return render_template('index.html', filename=filename)
 
 
--- procesamiento.py
+++ procesamiento.py
@@ -7,12 +7,13 @@ from matplotlib.pyplot import imshow, figure, subplot, show
 def crop(img, x, y, ancho, alto):
     image = cv2.imread(f"static/uploads/{img}")
     crop_image = image[y:y+alto, x:x+ancho]
-    cv2.imwrite(f"static/downloads/{img}", crop_image)
+    cv2.imwrite(f"static/uploads/{img}", crop_image)
     return 0
 
 
 def scale(img, porcentajeEscala):
+    image = cv2.imread(f"static/uploads/{img}")
     scale_image = cv2.resize(
-        f"static/uploads/{img}", None, fx=porcentajeEscala/100, fy=porcentajeEscala/100)
-    cv2.imwrite(f"downloads/{img}", scale_image)
+        image, None, fx=porcentajeEscala/100, fy=porcentajeEscala/100)
+    cv2.imwrite(f"static/uploads/{img}", scale_image)
     return 0
