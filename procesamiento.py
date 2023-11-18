14595121dabca1e5cfdcfada9c7eca0b440bffbf
Federico Rodriguez <federf2018@live.com>
2023-11-18 19:12:24 -0300

    feat: Object Detector

--- procesamiento.py
+++ procesamiento.py
@@ -1,4 +1,5 @@
 import cv2
+import requests
 from PIL import Image
 from matplotlib.pyplot import imshow, figure, subplot, show
 
@@ -19,4 +20,14 @@ def scale(img, porcentajeEscala):
         scale_image = cv2.resize(
             image, None, fx=porcentajeEscala/100, fy=porcentajeEscala/100, interpolation=cv2.INTER_AREA)
     cv2.imwrite(f"static/processing/{img}", scale_image)
+    return 0
+
+
+def objectDetector(img):
+    URL = 'http://localhost:32168/v1/vision/detection'
+
+    print(img)
+    image_data = open(f"static/processing/{img}", "rb").read()
+    response = requests.post(URL, files={"image": image_data}).json()
+    print(response['message'])
     return 0
